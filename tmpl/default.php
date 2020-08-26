6d93e4a7992a878ec3921d25ec17e2cb9a790a7f
Kushanrawindu <kushanrawindu@gmail.com>
2020-08-26 12:35:54 +0530

    add contact form

--- tmpl/default.php
+++ tmpl/default.php
@@ -1,5 +1,26 @@
 <?php
     defined('_JEXEC') or die;
-
-    echo "this is a test";
 ?>
+
+<div class="<?php echo $params->get('moduleclass_sfx');?>">
+    <div><?php echo $params->get('pretext');?></div>
+        
+        <form method="post" name="samplecontact_frm">
+            <fieldset>
+                <label for="name">Name</label>
+                <input type="text" id="name" name="name" style="width:95%;"/>
+
+                <label for="email">E-mail</label>
+                <input type="email" id="email" name="email" style="width:95%;"/>
+
+                <label for="message">Message</label>
+                <textarea cols="30" rows="5" style="width:95%;"></textarea>
+
+                <button type="submit">Submit</button>
+            </fieldset>
+
+            <input type="hidden" name="check" value="1"/>
+        </form>
+
+    <div><?php echo $params->get('posttext');?></div>
+</div>
